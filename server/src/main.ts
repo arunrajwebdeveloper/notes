9faa8e9cc7056aaef02fa2381954d08912660cf9
arunraj.r <arunraj.r@nergyindia.com>
2025-10-15 18:52:41 +0530

    fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+  // app.enableCors({
+  //   origin: ['http://localhost:3000'],
+  //   credentials: true, // Allow cookies
+  // });
   app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
