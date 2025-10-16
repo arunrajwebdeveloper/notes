4c2d03ea74e4199dd2119ec21f68d1f3b84ffdeb
arunraj.r <arunraj.r@nergyindia.com>
2025-10-16 19:07:15 +0530

    fix colors

--- client/src/components/NoteItem.tsx
+++ client/src/components/NoteItem.tsx
@@ -1,23 +1,26 @@
 import type { Note } from "../types/note.types";
 import { dateFormatter, trimText } from "../utils";
+import { noteColors } from "../utils/constants";
 
 function NoteItem({
   _id,
   title,
   description,
-  userId,
   orderIndex,
   color,
   isPinned,
   tags,
-  isArchived,
-  isTrash,
   createdAt,
-  updatedAt,
 }: Note) {
   return (
     <div className="basis-1 lg:basis-1/2 xl:basis-1/3 2xl:basis-1/4 p-2 xl:p-3 flex">
-      <div className="p-8 bg-lime-400 rounded-2xl overflow-hidden flex flex-col justify-between cursor-pointer w-full">
+      <div
+        style={{
+          backgroundColor: color,
+          border: color === "#ffffff" ? "1px solid #eee" : "0",
+        }}
+        className="p-8 rounded-2xl overflow-hidden flex flex-col justify-between cursor-pointer w-full"
+      >
         <div>
           <h2 className="text-2xl xl:text-3xl mb-4">{trimText(title, 80)}</h2>
           <p className="m-0 text-black text-sm xl:text-base">
@@ -42,6 +45,19 @@ function NoteItem({
               {dateFormatter(createdAt)}
             </span>
           </div>
+
+          {/* <div className="flex gap-1 flex-wrap mt-6">
+            {noteColors?.map((color: string) => (
+              <div
+                key={color}
+                style={{
+                  backgroundColor: color,
+                  border: color === "#ffffff" ? "1px solid #eee" : "0",
+                }}
+                className="w-8 h-8 rounded-full cursor-pointer"
+              ></div>
+            ))}
+          </div> */}
         </div>
       </div>
     </div>
---
+++ client/src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const noteColors = [
+  "#ffffff",
+  "#f46262",
+  "#cd6be9",
+  "#a280ff",
+  "#90a4f6",
+  "#59dbb1",
+  "#59db8a",
+  "#a5e658",
+  "#fef064",
+  "#feb766",
+  "#b6b6b6",
+  "#fafafa",
+];
