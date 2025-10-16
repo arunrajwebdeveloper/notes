8f210f87e5195fd93ac7a633bf9413bcc7271304
arunraj.r <arunraj.r@nergyindia.com>
2025-10-16 10:34:26 +0530

    client font

--- client/src/components/Note.tsx
+++ client/src/components/Note.tsx
@@ -1,7 +1,7 @@
 function Note() {
   return (
-    <div className="flex justify-between items-center bg-green-600 text-white p-4 w-80">
-      fdgfd
+    <div className="">
+      <h1>Designs</h1>
     </div>
   );
 }
