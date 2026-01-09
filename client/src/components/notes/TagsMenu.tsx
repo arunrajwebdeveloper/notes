bcda949c68d439c8bb937425498de472f3d10723
arunraj.r <arunraj.r@nergyindia.com>
2026-01-09 10:04:57 +0530

    fix

--- client/src/components/notes/TagsMenu.tsx
+++ client/src/components/notes/TagsMenu.tsx
@@ -2,6 +2,7 @@ import Toggler from "../common/Toggler";
 import Tooltip from "../common/Tooltip";
 import { CircleCheck, Tag } from "lucide-react";
 import type { Tag as TagType } from "../../types/note.types";
+import { useMemo } from "react";
 
 interface TagsMenuProps {
   tags: TagType[];
@@ -16,16 +17,27 @@ function TagsMenu({
   onChange,
   isLoading = false,
 }: TagsMenuProps) {
-  const onChooseTag = (tag: TagType) => {
-    // Check if tag is already selected
-    const isAlreadySelected = selectedTags.some((t) => t._id === tag._id);
+  const selectedTagIds = new Set(selectedTags.map((tag) => tag._id));
 
-    const updatedTags = isAlreadySelected
+  const onChooseTag = (tag: TagType) => {
+    const isSelected = selectedTagIds.has(tag._id);
+    const updatedTags = isSelected
       ? selectedTags.filter((t) => t._id !== tag._id)
       : [tag, ...selectedTags];
 
     onChange(updatedTags);
   };
+
+  const sortedTagList = useMemo(() => {
+    return [...tags].sort((a, b) => {
+      const aSelected = selectedTagIds.has(a._id);
+      const bSelected = selectedTagIds.has(b._id);
+
+      if (aSelected && !bSelected) return -1; // a first
+      if (!aSelected && bSelected) return 1; // b first
+      return 0; // keep same order otherwise
+    });
+  }, [tags, selectedTags]);
 
   return (
     <Toggler>
@@ -43,19 +55,20 @@ function TagsMenu({
         )}
       </Toggler.Toggle>
       <Toggler.Menu>
-        {({ closeMenu }) => (
+        {() => (
           <div
             className="space-y-1 w-55 h-full max-h-96 overflow-y-auto overflow-x-hidden mb-2 bg-white py-4 px-2 rounded-lg shadow-md border border-slate-200 
           [&::-webkit-scrollbar]:w-1
-    [&::-webkit-scrollbar-track]:bg-gray-100
-    [&::-webkit-scrollbar-thumb]:bg-gray-400
-    dark:[&::-webkit-scrollbar-track]:bg-neutral-400
-    dark:[&::-webkit-scrollbar-thumb]:bg-neutral-700
+        [&::-webkit-scrollbar-track]:bg-gray-100
+        [&::-webkit-scrollbar-thumb]:bg-gray-400
+        dark:[&::-webkit-scrollbar-track]:bg-neutral-400
+        dark:[&::-webkit-scrollbar-thumb]:bg-neutral-700
           "
           >
-            {tags?.length !== 0 ? (
-              tags?.map(({ _id, name }) => {
-                const isSelected = selectedTags.some((t) => t._id === _id);
+            {sortedTagList?.length !== 0 ? (
+              sortedTagList?.map(({ _id, name }) => {
+                const isSelected = selectedTagIds.has(_id);
+
                 return (
                   <div
                     key={`tags-menu-item-${_id}`}
