711507bc138af8e9a569d240c0dcafb56f457c88
arunraj.r <arunraj.r@nergyindia.com>
2026-02-17 09:11:56 +0530

    fix auto focus

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -17,7 +17,7 @@ function SearchBar({
     if (ref.current) {
       ref.current.focus();
     }
-  }, [localSearch, isLoading]);
+  }, [localSearch]);
   return (
     <div className="relative w-full max-w-sm hover:max-w-lg transition-all duration-300">
       <Search className="absolute w-5 h-5 text-gray-500 z-10 left-0 top-1/2 transform -translate-y-1/2 pointer-events-none" />
